fcaedf7b1f54ccf7d862071605abad6bd423037e
hoangleminh2709 <leminhlaso1@gmail.com>
2023-07-29 21:59:10 +0700

    prj3

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Blog from "./components/Blog";
+import About from "./components/About";
+// import React from "react";
+// import "./App.css";
+
+// function App() {
+//   return <div>1</div>;
+// }
+
+// export default App;
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Blog />
+    </div>
+  );
+};
+
+export default App;
